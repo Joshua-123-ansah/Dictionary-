979d538700db685bed6de6659a62f3cdb06e596e
joshuaowusuansah <owusuja500@gmail.com>
2021-01-20 05:22:49 -0800

    Added a GUI

--- MyDictionary.py
+++ MyDictionary.py
@@ -1,30 +1,60 @@
 import json
 from difflib import get_close_matches
+from os import name
+from tkinter import *
+
+window=Tk()
+window.title("Dictionary")
 data=json.load(open("data.json","r"))
-run=True
-while run:
-    word=input("Enter a word: ").lower()
+
+e=Entry(window,bg="#ABB8B8",width=50,borderwidth=5,font=("Times New Roman",13))
+t1=Text(window,height=15,width=48,borderwidth=2,bg="#ABB8B8",font=("Times New Roman",15))
+
+def word_meaning():
+    global word
+    word=str(e.get()).lower()
     if word in data.keys():
-        output1=data[word]
-        if type(output1)==list:
-            for i in output1:
-                print(i)
-        else:
-            print(output1)
-    elif len(get_close_matches(word,data.keys()))>0:
-        suggested_word=get_close_matches(word,data.keys())[0]
-        user_opinion=input("Did you mean %s instead. Enter Y if Yes and N if No: "%suggested_word).lower()
-        if user_opinion=="y":
-            output2=data[suggested_word]
-            if (output2)==list:
-                for i in output2:
-                    print(i)
-            else:
-                print(output2)
-        else:
-            print("Word not found")
-    else:
-        print("Word not found")
+        list_to_sentences()
   
+    elif len(get_close_matches(word,data.keys()))>0:
+        t1.delete(1.0,END)
+        suggested_word=get_close_matches(word,data.keys())[0]
+        global suggested_word_
+        suggested_word_=str(suggested_word)
+        word=suggested_word_
+        t1.insert(END,"Did you mean %s. If Yes press the Yes Button if No press the No Button: "%suggested_word_)
+
+def list_to_sentences():
+    output=data[word]
+    if type(output)==list:
+        for line in output:
+            t1.insert(END,line+"\n")
+    else:
+        t1.insert(END,output)
+
+def yes_button():
+    t1.delete(1.0,END)
+    list_to_sentences()
+
+def no_button():
+    t1.delete(1.0,END)
+    t1.insert(END, "Word not found")
+
+
+enter_button=Button(window,text="Enter",bg="#A9CCCC",padx=25,pady=6,command=word_meaning)
+yes_button=Button(window,text="Yes",bg="#A9CCCC",padx=25,pady=6,command=yes_button)
+no_button=Button(window,text="No",bg="#A9CCCC",padx=25,pady=6,command=no_button)
+
+e.grid(row=0,column=0,columnspan=3,padx=15)
+enter_button.grid(row=2,column=0)
+yes_button.grid(row=2,column=1)
+no_button.grid(row=2,column=2)
+t1.grid(row=1,column=0,columnspan=3)
+
+
+
+window.mainloop()
+
+
 
 
